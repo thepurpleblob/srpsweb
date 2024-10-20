a4fab6c96d15b717e39ab25eda78dacd5d25157e
Howard Miller <howardsmiller@gmail.com>
2024-10-20 17:52:24 +0100

    initial commit

---
+++ src/components/InMaintenance.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="absolute-bottom custom-caption">
+        <div class="text-h2">Undergoing maintenance...</div>
+        <div class="text-subtitle1">Site is undergoing maintenance and will be back soon.</div>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 12px
+  color: white
+  background-color: rgba(0, 0, 0, .3)
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-carousel
+    animated
+    v-model="slide"
+    arrows
+    infinite
+    height="800px"
+    :autoplay="autoplay"
+  >
+    <q-carousel-slide :name="1" img-src="srps7.jpg">
+      <InMaintenance></InMaintenance>
+    </q-carousel-slide>
+    <q-carousel-slide :name="2" img-src="srps8.jpg">
+      <InMaintenance></InMaintenance>
+    </q-carousel-slide>
+    <q-carousel-slide :name="3" img-src="srps9.jpg">
+      <InMaintenance></InMaintenance>
+    </q-carousel-slide>
+    <q-carousel-slide :name="4" img-src="srps10.jpg">
+      <InMaintenance></InMaintenance>
+    </q-carousel-slide>
+    <q-carousel-slide :name="5" img-src="srps4.jpg">
+      <InMaintenance></InMaintenance>
+    </q-carousel-slide>
+    <q-carousel-slide :name="6" img-src="srps2.jpg">
+      <InMaintenance></InMaintenance>
+    </q-carousel-slide>
+  </q-carousel>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import InMaintenance from '../components/InMaintenance.vue';
+
+const slide = ref(1);
+const autoplay = ref(true);
+
+defineOptions({
+  name: 'IndexPage',
+});
+</script>
+
