cc8566da527d491de79112063530f900d6751f29
Howard Miller <howardsmiller@gmail.com>
2024-10-26 10:58:44 +0100

    adding Clarity

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,37 @@
 <template>
-  <q-carousel
-    animated
-    v-model="slide"
-    arrows
-    infinite
-    height="800px"
-    :autoplay="autoplay"
-  >
-    <q-carousel-slide :name="1" img-src="srps7.jpg">
-      <InMaintenance></InMaintenance>
-    </q-carousel-slide>
-    <q-carousel-slide :name="2" img-src="srps8.jpg">
-      <InMaintenance></InMaintenance>
-    </q-carousel-slide>
-    <q-carousel-slide :name="3" img-src="srps9.jpg">
-      <InMaintenance></InMaintenance>
-    </q-carousel-slide>
-    <q-carousel-slide :name="4" img-src="srps10.jpg">
-      <InMaintenance></InMaintenance>
-    </q-carousel-slide>
-    <q-carousel-slide :name="5" img-src="srps4.jpg">
-      <InMaintenance></InMaintenance>
-    </q-carousel-slide>
-    <q-carousel-slide :name="6" img-src="srps2.jpg">
-      <InMaintenance></InMaintenance>
-    </q-carousel-slide>
-  </q-carousel>
+  <q-responsive :ratio="1" class="rt-slide">
+      <q-carousel
+          animated
+          v-model="slide"
+          arrows
+          infinite
+          height="800px"
+          :autoplay="autoplay"
+      >
+
+        <q-carousel-slide v-for="i in 6" :name="i" :key="i" :img-src="'srps' + i +'.jpg'">
+            <!-- <InMaintenance></InMaintenance> -->
+        </q-carousel-slide>
+
+      </q-carousel>
+  </q-responsive>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import InMaintenance from '../components/InMaintenance.vue';
+  import { ref } from 'vue';
+  //import InMaintenance from '../components/InMaintenance.vue';
 
-const slide = ref(1);
-const autoplay = ref(true);
+  const slide = ref(1);
+  const autoplay = ref(true);
 
-defineOptions({
-  name: 'IndexPage',
-});
+  defineOptions({
+      name: 'IndexPage',
+  });
 </script>
+
+<style lang="sass">
+.rt-slide
+  width: 100%
+</style>
+
 
