4be040c8fba18c8a2b78331134a4c20922cdab2f
Howard Miller <howardsmiller@gmail.com>
2024-10-27 16:50:38 +0000

    finally got home page links correct

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -14,90 +14,123 @@
         </div>
     </div>
 
-    <div class="row justify-evenly bg-white">
-        <q-card rounded class="srps-card">
-            <q-card-section>
-
-                <!-- BKR -->
-                <a href="https://www.bkrailway.co.uk" class="srps-link">
-                    <div class="row q-mt-md bg-green-9 q-px-md srps-redirect" @click="click_bkr">
-                        <div class="col-4 srps-rel">
-                            <img src="~assets/bklogo.png" class="srps-image absolute-center" />
-                        </div>
-                        <div class="col-8 q-py-md text-center">
-                            Travel on Scotland's Heritage railway by the Firth of Forth and
-                            take a trip on the train to Kinneil, Birkhill and Manuel,
-                            experiencing travel of a bygone era. Enjoy Afternoon Teas,
-                            dining trains, special galas, Santa trains or character days like
-                            Days Out With Thomas&trade; - there's something for everyone!
-                        </div>
+    <!-- BKR  -->
+    <a href="https://www.bkrailway.co.uk" class="srps-link">
+        <div class="column flex-center brand-bkr">
+            <div class="col srps-800">
+                <div class="row srps-redirect">
+                    <div class="col-4 srps-rel">
+                        <q-img src="~assets/bklogo.png" class="absolute-center" fit="scale-down"/>
                     </div>
-                </a>
-
-
-                <!-- Museum -->
-                <a href="https://museumofscottishrailways.org.uk/" class="srps-link">
-                    <div class="row bg-black text-white q-px-md srps-redirect">
-                        <div class="col-4 srps-rel">
-                            <img src="~assets/museum-logo.png" class="srps-image absolute-center" />
-                        </div>
-                        <div class="col-8 q-py-md text-center text-white">
-                            Located on a separate site near Bo'ness station, the Museum
-                            houses one of the largest collections of railway artefacts in
-                            the UK and promotes education of railways in Scotland and of
-                            the Scottish railway industry.
-                        </div>
+                    <div class="col-8">
+                        <q-img src="~assets/bkr-image512.png" />
                     </div>
-                </a>
+                </div>
+            </div>
+            <div class="col srps-800 text-center">
+                Travel on Scotland's Heritage railway by the Firth of Forth and
+                take a trip on the train to Kinneil, Birkhill and Manuel,
+                experiencing travel of a bygone era. Enjoy Afternoon Teas,
+                dining trains, special galas, Santa trains or character days like
+                Days Out With Thomas&trade; - there's something for everyone!
+            </div>
+        </div>
+    </a>
 
-                <!-- Railtours -->
-                <a href="https://www.srpsrailtours.co.uk/" class="srps-link">
-                    <div class="row bg-red text-white q-px-md srps-redirect">
-                        <div class="col-4 srps-rel">
-                            <img src="~assets/Railtours.png" class="srps-image absolute-center" />
-                        </div>
-                        <div class="col-8 q-py-md text-center text-white">
-                            Britain's longest established operator of scenic railway
-                            excursions, SRPS Railtours Limited offers excursions to view the
-                            splendour of Scotland, visit historic towns and cities or enjoy
-                            heritage steam and diesel traction around the UK mainline in
-                            partnership with the Royal Scot General Trust and Locomotive
-                        </div>
+    <!-- Museum  -->
+    <a href="https://museumofscottishrailways.org.uk/" class="srps-link">
+        <div class="column flex-center brand-museum">
+            <div class="col srps-800">
+                <div class="row srps-redirect">
+                    <div class="col-4 srps-rel">
+                        <q-img src="~assets/museum-logo.png" class="absolute-center" fit="scale-down"/>
                     </div>
-                </a>
+                    <div class="col-8">
+                        <q-img src="~assets/museum512.png" />
+                    </div>
+                </div>
+            </div>
+            <div class="col srps-800 text-center">
+                Located on a separate site near Bo'ness station, the Museum
+                houses one of the largest collections of railway artefacts in
+                the UK and promotes education of railways in Scotland and of
+                the Scottish railway industry.
+            </div>
+        </div>
+    </a>
 
-            </q-card-section>
-        </q-card>
-    </div>
+    <!-- Railtours  -->
+    <a href="https://www.srpsrailtours.co.uk/" class="srps-link">
+        <div class="column flex-center brand-railtours">
+            <div class="col srps-800">
+                <div class="row srps-redirect">
+                    <div class="col-4 srps-rel">
+                        <q-img src="~assets/Railtours.png" class="absolute-center" fit="scale-down"/>
+                    </div>
+                    <div class="col-8">
+                        <q-img src="~assets/railtours512.png" />
+                    </div>
+                </div>
+            </div>
+            <div class="col srps-800 text-center">
+                Britain's longest established operator of scenic railway
+                excursions, SRPS Railtours Limited offers excursions to view the
+                splendour of Scotland, visit historic towns and cities or enjoy
+                heritage steam and diesel traction around the UK mainline in
+                partnership with the Royal Scot General Trust and Locomotive
+            </div>
+        </div>
+    </a>
+
+    <!-- Shop  -->
+    <a href="https://www.srpsrailtours.co.uk/" class="srps-link">
+        <div class="column flex-center bg-white text-black">
+            <div class="col srps-800">
+                <div class="row srps-redirect">
+                    <div class="col-4 srps-rel">
+                        <q-img src="~assets/shop_logo.png" class="absolute-center" fit="scale-down"/>
+                    </div>
+                    <div class="col-8 q-pt-lg">
+                        The SRPS online shop offers items for sale that are relevant to
+                        Scottish Railway history such as books and paintings. It also
+                        offers exclusive limited edition models of locomotives or rolling
+                        stock that form part of the SRPS collection that you can own and
+                        operate on your own model railway.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+
 </template>
 
-<script setup lang="ts">
-
-    /**
-     * Redirect to BKR
-     */
-    function click_bkr() {
-        window.location.replace('https://www.bkrailway.org.uk');
-    }
-
-</script>
 
 <style lang="sass">
+.brand-bkr
+    background: #49853b
+    color: white
+.brand-museum
+    background: black
+    color: white
+.brand-railtours
+    background: #6b0319
+    color: #fddf51
 .srps-link
     color: inherit
     text-decoration: inherit
+    font-size: 120%
 .srps-redirect
+    min-height: 200px
+    max-width: 800px
+.srps-800
+    max-width: 800px
+    width: 100%
     min-height: 200px
 .srps-preamble
     max-width: 640px
     width: 100%
     font-size: 120%
-.srps-card
-    max-width: 900px
-    width: 100%
-    font-size: 120%
-.srps-image
-    max-width: 100%
 .srps-rel
     position: relative
+
 </style>
