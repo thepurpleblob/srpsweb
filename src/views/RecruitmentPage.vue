941758bf15f144187c68e56ed764db17902b6047
Howard Miller <howardsmiller@gmail.com>
2026-02-18 14:10:04 +0000

    missing ts declaration

--- src/views/RecruitmentPage.vue
+++ src/views/RecruitmentPage.vue
@@ -34,7 +34,7 @@
 
 </template>
 
-<script setup>
+<script setup lang="ts">
     import MarketingManagerAdvertURL from '../assets/MarketingManagerAdvert.pdf';
     import MarketingManagerJDURL from '../assets/MarketingManagerJD.pdf';
     import PDFIconURL from '../assets/pdficon.png';
